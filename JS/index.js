5239405aa5a3f25e459ca8bdfd15339702158e60
LautaroLeall <lautaroleal4@gmail.com>
2024-11-21 21:00:28 -0300

    fragancia.html

--- JS/index.js
+++ JS/index.js
@@ -1,134 +1,128 @@
-const openFragancias = document.querySelector(".fragancias");
-const closeFragancias = document.querySelector(".closefragancias");
-const list = document.querySelector(".list");
-const listCard = document.querySelector(".listCard");
-const total = document.querySelector(".total");
-const body = document.querySelector("body");
-const quantity = document.querySelector(".quantity");
+const openShopping = document.querySelector(".shopping"),
+    closeShopping = document.querySelector(".closeShopping"),
+    body = document.querySelector("body"),
+    list= document.querySelector(".list"),
+    listCard = document.querySelector(".listCard"),
+    total = document.querySelector(".total"),
+    quantity = document.querySelector(".quantity")
 
-openFragancias.addEventListener("click", () => {
+
+openShopping.addEventListener("click", () => {
     body.classList.add("active");
-});
+})
 
-closeFragancias.addEventListener("click", () => {
-    body.classList.remove("active");
-});
+closeShopping.addEventListener("click", () => {
+    body.classList.remove("active")
+})
 
 let products = [
     {
-        id: 1,
-        name: "Fragancia 1",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 10,
-        
+        "id": 1,
+        "name": "PRODUCT 1",
+        "image":"1.png",
+        "price": 2000
     },
     {
-        id: 2,
-        name: "Fragancia 2",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 20,
-       
+        "id": 2,
+        "name": "PRODUCT 2",
+        "image":"2.png",
+        "price": 2200
     },
     {
-        id: 3,
-        name: "Fragancia 3",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 30,
-       
+        "id": 3,
+        "name": "PRODUCT 3",
+        "image":"3.png",
+        "price": 2400
     },
     {
-        id: 4,
-        name: "Fragancia 4",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 40,
-        
+        "id": 4,
+        "name": "PRODUCT 4",
+        "image":"4.png",
+        "price": 2600
     },
     {
-        id: 5,
-        name: "Fragancia 5",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 50,
-        
+        "id": 5,
+        "name": "PRODUCT 5",
+        "image":"5.png",
+        "price": 1400
     },
     {
-        id: 6,
-        name: "Fragancia 6",
-        images: "../assets/images/imgbleudechanel.webp",
-        price: 60,
-        
-    },  
+        "id": 6,
+        "name": "PRODUCT 6",
+        "image":"6.png",
+        "price": 1800
+    }
+]
 
-];
 
-let listCards = []; 
- 
+let listCards = [];
+
 const initApp = () => {
-    products.forEach((value,key) => {
+    products.forEach((value, key) => {
         let newDiv = document.createElement("div");
         newDiv.classList.add("item");
-        newDiv.innerHTML = 
-        `
-        <img src="img/${value.images}">
-        <div class="title">${value.name}</div>
-        <div class="price">$${value.price.toLocaleString()}</div>
-        <button onclick="addToCart(${key})">Add to cart</button>
+        newDiv.innerHTML = `
+            <img src = "img/${value.image}">
+            <div class = "title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick = "addToCard(${key})">Add To Card</button>
         `;
-        list.appendChild(newDiv);
-    });
-};
+        list.appendChild(newDiv)
+    })
+}
 
 initApp()
 
 
-const addToCart = (key) => {
-    if(listCards[key] == null){
+const addToCard = key => {
+    if(listCards[key] == null) {
         listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        // console.log(listCards);
         listCards[key].quantity = 1;
+        // console.log(listCards[key].quantity);
     }
 
-    reloadCard();
-};
+    reloadCard()
+}
 
 const reloadCard = () => {
     listCard.innerHTML = "";
     let count = 0;
-    let totalPrice = 0;
-    listCards.forEach((value,key) => {
-        totalPrice = totalPrice + value.price;
+    let totalPrice= 0;
+
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price
         count = count + value.quantity;
 
-        if (value != null){
+        if(value != null) {
             let newDiv = document.createElement("li");
             newDiv.innerHTML = `
-            <div><img src="img/${value.images}"></div>
-            <div class="cardTitle">${value.name}</div>
-            <div class="cardPrice">$${value.price.toLocaleString()}</div>
+                <div><img src = "img/${value.image}"></div>
+                <div class = "cardTitle">${value.name}</div>
+                <div class = "cardPrice">${value.price.toLocaleString()}</div>
 
-            <div>
-             <button style="background-color: #560bad
-             class="cardButton" onclick= "changeQuantity (${key}),${value.quantity - 1}"
-             >-</button>
-            <div class="count">${count}</div>
-               <button style="background-color: #560bad
-             class="cardButton" onclick= "changeQuantity (${key}),${value.quantity + 1}"
-             >-</button>
-            </div>
-
-            list.appendChild(newDiv);
+                <div>
+                    <button style = "background-color:#560bad;" class = "cardButton" onclick = "changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class = "count">${value.quantity}</div>
+                    <button style = "background-color:#560bad;" class = "cardButton" onclick = "changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
             `
+            listCard.appendChild(newDiv)
         }
-        total.innerHTML = totalPrice.toLocaleString();
-        quantity.innertext = count;
-    })
 
-};
+        total.innerText = totalPrice.toLocaleString();
+        quantity.innerText = count;
+    })
+}
+
 
 const changeQuantity = (key, quantity) => {
-  if (quantity == 0) {
-    delete listCards[key];
-  } else {
-    listCards[key].quantity = quantity;
-    listCards[key].price = quantity * products[key].price;
-  }
-  reloadCard();
-};
+    if(quantity == 0) {
+        delete listCards[key]
+    }
+    else {
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price
+    }
+    reloadCard()
+}
